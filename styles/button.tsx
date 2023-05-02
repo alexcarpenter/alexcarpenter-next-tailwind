d3f0be990f1068a8d266271e97371fd93e257613
Alex Carpenter <im.alexcarpenter@gmail.com>
2023-05-02 08:44:39 -0400

    force button color

--- styles/button.tsx
+++ styles/button.tsx
@@ -1,18 +1,21 @@
 import { cva, type VariantProps } from "@/lib/classnames";
 
 export type ButtonVariantProps = VariantProps<typeof buttonVariants>;
-export const buttonVariants = cva("font-medium text-sm rounded-md", {
-  variants: {
-    intent: {
-      action: "bg-surface-action",
+export const buttonVariants = cva(
+  "font-medium text-sm rounded-md text-[#171717]",
+  {
+    variants: {
+      intent: {
+        action: "bg-surface-action",
+      },
+      size: {
+        small: "px-2.5 py-1.5",
+        medium: "px-4 py-2.5",
+      },
     },
-    size: {
-      small: "px-2.5 py-1.5",
-      medium: "px-4 py-2.5",
+    defaultVariants: {
+      intent: "action",
+      size: "medium",
     },
-  },
-  defaultVariants: {
-    intent: "action",
-    size: "medium",
-  },
-});
+  }
+);
