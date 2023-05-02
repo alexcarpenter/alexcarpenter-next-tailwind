79e1f372317c90853e7e0902121f4347a430b2a0
Alex Carpenter <im.alexcarpenter@gmail.com>
2023-05-02 08:57:57 -0400

    column position

--- app/footer.tsx
+++ app/footer.tsx
@@ -2,8 +2,8 @@ import { ThemeSwitch } from "./theme-switch";
 export function Footer() {
   return (
     <footer className="pt-16">
-      <div className="grid gap-16 md:grid-cols-4">
-        <div>
+      <div className="grid gap-x-16 gap-y-4 md:grid-cols-4">
+        <div className="md:col-start-2">
           <ThemeSwitch />
         </div>
 
