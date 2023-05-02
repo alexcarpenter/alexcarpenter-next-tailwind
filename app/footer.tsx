fdf375b63712fbf63c61638f16db4f51dd2a38b9
Alex Carpenter <im.alexcarpenter@gmail.com>
2023-05-02 08:55:56 -0400

    scaffold theme select

--- app/footer.tsx
+++ app/footer.tsx
@@ -1,7 +1,12 @@
+import { ThemeSwitch } from "./theme-switch";
 export function Footer() {
   return (
     <footer className="pt-16">
-      <div className="grid md:grid-cols-4 gap-16">
+      <div className="grid gap-16 md:grid-cols-4">
+        <div>
+          <ThemeSwitch />
+        </div>
+
         <div className="md:col-start-4">
           <p className="text-foreground">“Less but better”</p>
           <p className="text-foreground-neutral">
