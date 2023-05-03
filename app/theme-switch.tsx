b0dc04ebd67f64aef29bb04710c8eac0d998a7ec
Alex Carpenter <im.alexcarpenter@gmail.com>
2023-05-03 08:36:09 -0400

    add icons

--- app/theme-switch.tsx
+++ app/theme-switch.tsx
@@ -30,7 +30,8 @@ export function ThemeSwitch() {
           defaultChecked={theme === "system"}
           onChange={handleChange}
         />
-        <span>System</span>
+        <span className="sr-only">System</span>
+        <SystemIcon />
       </label>
       <label>
         <input
@@ -40,7 +41,8 @@ export function ThemeSwitch() {
           defaultChecked={theme === "dark"}
           onChange={handleChange}
         />
-        <span>Dark</span>
+        <span className="sr-only">Dark</span>
+        <DarkIcon />
       </label>
       <label>
         <input
@@ -50,8 +52,76 @@ export function ThemeSwitch() {
           defaultChecked={theme === "light"}
           onChange={handleChange}
         />
-        <span>Light</span>
+        <span className="sr-only">Light</span>
+        <LightIcon />
       </label>
     </fieldset>
+  );
+}
+
+function SystemIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="feather feather-monitor"
+    >
+      <rect x="2" y="3" width="20" height="14" rx="2" ry="2" />
+      <line x1="8" y1="21" x2="16" y2="21" />
+      <line x1="12" y1="17" x2="12" y2="21" />
+    </svg>
+  );
+}
+
+function LightIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="feather feather-sun"
+    >
+      <circle cx="12" cy="12" r="5" />
+      <line x1="12" y1="1" x2="12" y2="3" />
+      <line x1="12" y1="21" x2="12" y2="23" />
+      <line x1="4.22" y1="4.22" x2="5.64" y2="5.64" />
+      <line x1="18.36" y1="18.36" x2="19.78" y2="19.78" />
+      <line x1="1" y1="12" x2="3" y2="12" />
+      <line x1="21" y1="12" x2="23" y2="12" />
+      <line x1="4.22" y1="19.78" x2="5.64" y2="18.36" />
+      <line x1="18.36" y1="5.64" x2="19.78" y2="4.22" />
+    </svg>
+  );
+}
+
+function DarkIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="feather feather-moon"
+    >
+      <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z" />
+    </svg>
   );
 }
