766f5a948401235ac8c7135ee4db2ee9673b0883
Alex Carpenter <im.alexcarpenter@gmail.com>
2023-05-04 07:39:04 -0400

    extract links

--- app/header.tsx
+++ app/header.tsx
@@ -1,5 +1,35 @@
 import Link from "next/link";
 
+const routes = [
+  {
+    label: "index",
+    href: "/",
+  },
+  {
+    label: "posts",
+    href: "/posts",
+  },
+  {
+    label: "bookmarks",
+    href: "/bookmarks",
+  },
+];
+
+const accounts = [
+  {
+    label: "Twitter",
+    href: "https://twitter.com/hybrid_alex",
+  },
+  {
+    label: "GitHub",
+    href: "https://github.com/alexcarpenter",
+  },
+  {
+    label: "LinkedIn",
+    href: "https://www.linkedin.com/in/imalexcarpenter",
+  },
+];
+
 export function Header() {
   return (
     <header className="grid grid-cols-2 gap-x-16 gap-y-4 md:grid-cols-4">
@@ -12,46 +42,33 @@ export function Header() {
 
       <div className="md:col-span-2">
         <p className="font-variable-semibold text-foreground">Routes</p>
-        <Link href="/" className="text-foreground-neutral">
-          <span aria-hidden="true">/</span>index
-        </Link>
-        <br />
-        <Link href="/posts" className="text-foreground-neutral">
-          <span aria-hidden="true">/</span>posts
-        </Link>
-        <br />
-        <Link href="/bookmarks" className="text-foreground-neutral">
-          <span aria-hidden="true">/</span>bookmarks
-        </Link>
-        {/* <br />
-        <Link href="/activity" className="text-foreground-neutral">
-          <span aria-hidden="true">/</span>activity
-        </Link>
-        <br />
-        <Link href="/colphon" className="text-foreground-neutral">
-          <span aria-hidden="true">/</span>colophon
-        </Link> */}
+        {routes.map(({ href, label }) => {
+          return (
+            <Link
+              key={href}
+              href={href}
+              className="block w-max text-foreground-neutral"
+            >
+              <span aria-hidden="true">/</span>
+              {label}
+            </Link>
+          );
+        })}
       </div>
 
       <div>
         <p className="font-variable-semibold text-foreground">Connect</p>
-        <a href="https://twitter.com/home" className="text-foreground-neutral">
-          Twitter <span aria-hidden="true">↗</span>
-        </a>
-        <br />
-        <a
-          href="https://github.com/alexcarpenter"
-          className="text-foreground-neutral"
-        >
-          GitHub <span aria-hidden="true">↗</span>
-        </a>
-        <br />
-        <a
-          href="https://www.linkedin.com/in/imalexcarpenter/"
-          className="text-foreground-neutral"
-        >
-          LinkedIn <span aria-hidden="true">↗</span>
-        </a>
+        {accounts.map(({ href, label }) => {
+          return (
+            <a
+              key={href}
+              href={href}
+              className="block w-max text-foreground-neutral"
+            >
+              {label} <span aria-hidden="true">↗</span>
+            </a>
+          );
+        })}
       </div>
     </header>
   );
